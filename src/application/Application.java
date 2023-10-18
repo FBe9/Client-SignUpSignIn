e123be757a44cff6be09596137b3b3ad7945dad6
Nerea <nereaoceja2003@gmail.com>
2023-10-18 12:24:16 +0200

    First commit with the windows fxml and the packages

---
+++ src/application/Application.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2dam
+ */
+public class Application extends javafx.application.Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+       
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
